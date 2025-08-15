b5a1de31563c5e320a9eb683ee95dac1d9f23b64
marianyarj <marianyarj@gmail.com>
2025-08-15 23:26:39 +0200

    fix: update navbar for lg screens and correct btn hover

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,14 +5,14 @@ const NavBar = () => {
     const pageLocation = useLocation();
     return (
         <>
-            <nav className=' items-center  box-border w-full flex flex-col md:items-start md:place-items-start md:grid md:grid-cols-6 xl:grid-cols-12'>
-                <h1 className='text-left text-4xl font-lato text-mustard md:text-6xl md:col-start-1 md:col-end-3 md:row-start-1'>
-                    <span className='font-lavishly text-7xl md:text-9xl'>C</span>artas
+            <nav className=' items-center  box-border w-full flex flex-col md:items-start md:place-items-start md:grid md:grid-cols-6 md:text-2xl xl:grid-cols-12'>
+                <h1 className='drop-shadow text-left text-4xl font-lato text-mustard md:text-6xl md:col-start-1 md:col-end-3 md:row-start-1'>
+                    <span className='font-lavishly text-7xl md:text-9xl drop-shadow-lg'>C</span>artas
                 </h1>
-                <h1 className='text-4xl font-lato text-mustard md:text-6xl md:col-start-2 md:row-start-2 xl:col-end-2 '><span className='font-lavishly text-7xl col-start-3 md:text-9xl'>S</span>ábias</h1>
+                <h1 className='drop-shadow text-4xl font-lato text-mustard md:text-6xl md:col-start-2 md:row-start-2 xl:col-end-2 '><span className='font-lavishly text-7xl col-start-3 md:text-9xl drop-shadow-lg'>S</span>ábias</h1>
                 <ul className='flex flex-col gap-8 mt-12 md:flex md:flex-row md:row-start-1 md:gap-4 md:col-end-8 xl:col-start-[-3] xl:row-start-2  2xl:col-end-[-5]'>
-                    <li><Link to="/" className={`hover:bg-midnight hover:text-mustard cursor-pointer py-3 px-6 whitespace-nowrap text-lg rounded-lg ${pageLocation.pathname === "/" ? "bg-mustard border-2 border-solid border-mustard text-midnight font-bold" : "border-2 border-solid border-mustard  text-mustard font-bold"} `}>Todas las cartas</Link></li>
-                    <li><Link to="/tarotreading" className={`hover:bg-midnight cursor-pointer py-3 px-6 whitespace-nowrap text-lg rounded-lg ${pageLocation.pathname === "/tarotreading" ? "bg-mustard border-2 border-solid border-mustard text-midnight font-bold" : "bg-sea/90 border-2 border-solid border-mustard text-mustard font-bold"} `}>Hacer Lectura</Link></li>
+                    <li><Link to="/" className={`hover:bg-midnight hover:text-mustard cursor-pointer py-3 px-6 whitespace-nowrap text-lg md:text-2xl rounded-lg ${pageLocation.pathname === "/" ? "bg-mustard border-2 border-solid border-mustard text-midnight font-bold" : "border-2 border-solid border-mustard  text-mustard font-bold"} `}>Todas las cartas</Link></li>
+                    <li><Link to="/tarotreading" className={`hover:bg-midnight hover:text-mustard cursor-pointer py-3 px-6 whitespace-nowrap text-lg md:text-2xl rounded-lg ${pageLocation.pathname === "/tarotreading" ? "bg-mustard border-2 border-solid border-mustard text-midnight font-bold" : "bg-sea/90 border-2 border-solid border-mustard text-mustard font-bold"} `}>Hacer Lectura</Link></li>
                 </ul>
             </nav>
         </>
