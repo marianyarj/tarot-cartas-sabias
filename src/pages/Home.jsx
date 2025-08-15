7cc8deccbed67dfaf5c2b31a6555c8e545d658e5
marianyarj <marianyarj@gmail.com>
2025-08-15 13:43:17 +0200

    style: update home and navbar styles for better visibility

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -26,8 +26,8 @@ function Home() {
     }
     return (
         <>
-            <section className='mt-18'>
-                <div className='bg-midnight text-mustard text-xl pt-8 pb-8 space-y-4 px-12 -mx-12 md:text-2xl md:-mx-20 xl:-mx-80 md:px-20 xl:px-80'>
+            <section className='w-full mt-18'>
+                <div className='bg-midnight text-mustard py-12 space-y-4 px-12 -mx-12 md:text-2xl md:-mx-20 xl:-mx-80 md:py-16 md:px-20 xl:px-80 xl:py-24'>
                     <h2 >
                         Ven a conocer el mundo de las mujeres que han dejado huella y siguen haciendo historia en la tecnología y ciencia.</h2>
                     <h2 >Con Cartas Sábias podrás explorar una baraja de tarot para descubrir a estas Diosas contemporáneas.</h2>
