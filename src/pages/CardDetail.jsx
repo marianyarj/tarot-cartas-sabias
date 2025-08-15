22020f6f56611729695ad6b061a827b89d899aa2
marianyarj <marianyarj@gmail.com>
2025-08-15 23:21:46 +0200

    feat: create cardDetail component and add in home

--- src/pages/CardDetail.jsx
+++ src/pages/CardDetail.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState, useRef } from 'react'
 import { getCardById } from '../services/cardTarotServices'
 import { useParams } from 'react-router-dom';
+import cardBack from '../assets/cardback.svg'
 
 function CardDetail() {
     const { id } = useParams()
@@ -28,10 +29,51 @@ function CardDetail() {
 
     return (
         <>
-            <div ref={cardDetailRef}>
-                <h2>{card.arcaneName}</h2>
-            </div>
+            <section ref={cardDetailRef} className='grid grid-auto-rows bg-midnight text-mustard mt-18 py-20 space-y-4 px-20 -mx-20 md:text-2xl md:-mx-40 lg:mx-60 xl:-mx-80 md:py-16 md:px-40 xl:px-80 xl:py-24'>
+                <h2 className='row-start-1 text-center text-3xl md:text-5xl [word-spacing:0.4em] col-span-full'>
+                    {card.arcaneName.split(" ").map((word, i) => (
+                        <span key={i}>
+                            <span className='font-lavishly text-6xl md:text-8xl'>{word.charAt(0)}</span>
+                            {word.slice(1)}{" "}
+                        </span>
+                    ))}
+                </h2>
 
+                <article className='row-start-2 flex flex-col md:flex-row items-center gap-6 mt-8'>
+                    <div className='flex flex-col items-center space-y-2'>
+                        <h3 className='text-2xl'>Arcano {card.id}</h3>
+                        <img src={card.arcaneImage.imageSrc} alt={card.arcaneName} className='w-3xs rounded-lg border-4 border-solid border-mustard' />
+                    </div>
+                    <p className='flex-1 mt-4  md:text-xl md:ml-6 2xl:text-2xl '>{card.arcaneDescription}</p>
+                </article>
+
+                <article className='row-start-3 flex flex-col-reverse md:flex-row items-center gap-6 mt-4'>
+                    <p className='flex-1 mt-4 text-end md:text-xl md:mr-6 2xl:text-2xl'>{card.goddessDescription}</p>
+                    <div className='flex flex-col items-center space-y-2 mt-4'>
+                        <h3 className='text-2xl'>{card.goddessName}</h3>
+                        <div className="relative w-3xs mx-auto">
+                            <img src={cardBack} alt={card.goddessName} className="w-3xs" />
+                            <img
+                                src={card.goddessImage.imageSrc}
+                                alt={card.goddessName}
+                                className="absolute top-51 left-1/2 w-60 h-64 rounded-full border-4 border-solid border-mustard -translate-x-1/2 -translate-y-1/2 object-cover object-top"
+                            />
+                        </div>
+                    </div>
+                </article>
+                <div className='mt-8 md:mt-12 xl:mt-16 text-center'>
+                    <button
+                        onClick={() => {
+                            const allCards = document.getElementById("allCards");
+                            if (allCards) allCards.scrollIntoView({ behavior: "smooth" });
+                        }}
+                        className='bg-mustard text-midnight hover:bg-midnight hover:text-mustard font-bold border-2 border-solid border-mustard cursor-pointer py-3 px-6 rounded-lg'
+                    >
+                        Conocer otra Diosa
+                    </button>
+                </div>
+
+            </section>
         </>
     )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -27,14 +27,17 @@ function Home() {
     return (
         <>
             <section className='w-full mt-18'>
-                <div className='bg-midnight text-mustard py-12 space-y-4 px-12 -mx-12 md:text-2xl md:-mx-20 xl:-mx-80 md:py-16 md:px-20 xl:px-80 xl:py-24'>
+                <div className='bg-midnight text-mustard py-20 space-y-4 px-20 -mx-20 md:text-2xl md:-mx-20 xl:-mx-80 md:py-16 md:px-20 xl:px-80 xl:py-24'>
                     <h2 >
                         Ven a conocer el mundo de las mujeres que han dejado huella y siguen haciendo historia en la tecnología y ciencia.</h2>
                     <h2 >Con Cartas Sábias podrás explorar una baraja de tarot para descubrir a estas Diosas contemporáneas.</h2>
                     <h2 > Solo tienes que hacer clic en una carta para conocer más detalles sobre ella.</h2>
                 </div>
             </section>
-            <CardGrid cards={cardsTarot} onCardClick={cardClick} showName={true} />
+            <div id="allCards">
+                <CardGrid cards={cardsTarot} onCardClick={cardClick} showName={true} />
+            </div>
+
 
             <Outlet />
         </>
