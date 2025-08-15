a89d713659ebd65fb06f5ae2e9eb5623021576fd
marianyarj <marianyarj@gmail.com>
2025-08-15 13:38:47 +0200

    feat: configure particles background

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,11 +1,56 @@
 import { Outlet } from "react-router-dom";
 import NavBar from "../components/NavBar";
 import FooterBar from "../components/FooterBar";
+import { useEffect, useState, useMemo } from "react";
+import Particles, { initParticlesEngine } from "@tsparticles/react";
+import { loadSlim } from "@tsparticles/slim";
 
 function Layout() {
+    const [init, setInit] = useState(false);
+
+    useEffect(() => {
+        initParticlesEngine(async (engine) => {
+            await loadSlim(engine);
+        }).then(() => setInit(true));
+    }, []);
+
+    const options = useMemo(() => ({
+        particles: {
+            number: {
+                value: 300,
+                density: { enable: true, value_area: 1200 }
+            },
+            shape: { type: ["circle", "star"] },
+            color: { value: ["#FFE5B5", "#B3773F"] },
+            size: { value: { min: 1, max: 4 } },
+            opacity: { value: { min: 0.2, max: 0.9 } },
+            move: {
+                enable: true,
+                speed: 0.5,
+                random: true,
+                straight: false,
+                out_mode: "out",
+            }
+        },
+        interactivity: {
+            events: {
+                onHover: { enable: true, mode: "attract" },
+                onClick: { enable: false }
+            },
+            modes: {
+                distance: 3, duration: 0.1
+            }
+
+        },
+        retina_detect: true
+    }), []);
+
+    if (!init) return null;
+
     return (
         <>
-            <div className="p-12 md:p-20 xl:pl-80 xl:pr-80 overflow-x-hidden">
+            <Particles className="fixed top-0 left-0 w-full h-screen -z-10" options={options} />
+            <div className="relative z-10 w-full p-12 md:p-20 xl:pl-80 xl:pr-80">
                 <NavBar />
                 <Outlet />
                 <FooterBar />
