030dfec91452b6be6a11b87ae763f5dc57e3d847
marianyarj <marianyarj@gmail.com>
2025-08-20 12:41:11 +0200

    feat: add footer text and styles

--- src/components/FooterBar.jsx
+++ src/components/FooterBar.jsx
@@ -2,7 +2,19 @@ import React from 'react'
 
 const FootBar = () => {
     return (
-        <>FootBar</>
+        <>
+            <div className='border-t-2 border-mustard bg-midnight py-12 -mx-20 md:-mx-40 lg:mx-60 xl:-mx-80'>
+                <section className='md:text-2xl text-mustard font-bold text-center'>
+                    <p>Este proyecto ha sido desarrollado como parte del programa de formación de <a href="https://factoriaf5.org/" target="_blank">FactoriaF5</a></p>
+                    <p>© 2025 <a href="https://github.com/marianyarj" target="_blank">Mariany</a> | <a href="https://github.com/CeliaMi/stem-tarot/blob/main/README.md" target="_blank">Tarot STEM</a></p>
+                    <div className='flex justify-center'>
+                        <img className='w-30 mt-4'
+                            src="https://femcoders.factoriaf5.org/wp-content/uploads/2021/12/Logo-FemCodersF5-color-300x224.png"
+                            alt="logo femcoders factoriaf5" />
+                    </div>
+                </section>
+            </div>
+        </>
     )
 }
 
