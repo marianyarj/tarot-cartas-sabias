2d32b0247a4584ef6682413080b9f85121ab85cf
marianyarj <marianyarj@gmail.com>
2025-08-20 15:20:28 +0200

    style: use 3 rows grid with auto and full screen in layout

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -50,7 +50,7 @@ function Layout() {
     return (
         <>
             <Particles className="fixed top-0 left-0 h-screen -z-10" options={options} />
-            <div className="relative z-10 w-full px-20 md:px-40 lg:pl-60 xl:pl-80 xl:pr-80">
+            <div className="relative z-10 grid grid-rows-[auto_1fr_auto] min-h-screen w-full px-20 md:px-40 lg:pl-60 xl:pl-80 xl:pr-80">
                 <NavBar />
                 <Outlet />
                 <FooterBar />
