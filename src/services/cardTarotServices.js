2cb6bda88e61051a96d37fcd3431bcbf5c4f63a4
marianyarj <marianyarj@gmail.com>
2025-08-21 10:04:59 +0200

    refactor: remove random functions from the services file

--- src/services/cardTarotServices.js
+++ src/services/cardTarotServices.js
@@ -18,3 +18,29 @@ export const getCardById = async (id) => {
         console.log(error + ">> error en Services");
     }
 }
+
+/*export const getOneCardRandom = async () => {
+    try {
+        const response = await axios.get(URL_API);
+        const cards = response.data;
+        const randomCard = Math.floor(Math.random() * cards.length);
+        return cards[randomCard];
+
+    } catch (error) {
+        console.log(error + ">> error en Services Random");
+    }
+}
+
+export const getAllCardsRandom = async () => {
+    try {
+        const response = await axios.get(URL_API);
+        const cards = response.data.slice();
+        for (let i = 0; i < cards.length; i++) {
+            const j = Math.floor(Math.random() * cards.length);
+            [cards[i], cards[j]] = [cards[j], cards[i]];
+        }
+        return cards;
+    } catch (error) {
+        console.log(error + ">> error en Sercices Random")
+    }
+}*/
