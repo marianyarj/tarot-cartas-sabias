c8130db4be10109026eba61ecb07e7dffdb9f15a
marianyarj <marianyarj@gmail.com>
2025-08-21 13:51:41 +0200

    feat: add detailArcane component to TarotReading

--- src/pages/TarotReading.jsx
+++ src/pages/TarotReading.jsx
@@ -1,13 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import { getAllCards } from '../services/cardTarotServices'
-import { useNavigate, Outlet } from 'react-router-dom';
-
-
+import CardDetailArcane from '../components/CardDetailArcane';
 
 function TarotReading() {
     const [availableCards, setAvailableCards] = useState([]);
-    const [cards, setCards] = useState([null, null, null]); // 3 espaços
-    const navigate = useNavigate();
+    const [cards, setCards] = useState([null, null, null]);
     const titleCard = ["pasado", "presente", "futuro"];
 
     const getAllCardsRandom = async () => {
@@ -36,17 +33,9 @@ function TarotReading() {
     }, []);
 
     const handleClick = (idx) => {
-        // if (cards[idx]) {
-        //     // Se a carta já foi escolhida, navegar para o detalhe
-        //     navigate(`/carddetail/${idx}`);
-        //     return;
-        // }
-
-        // Se ainda não foi escolhida, pegar uma carta aleatória
         const remainingCards = availableCards.filter(c => !cards.includes(c));
         console.log("remainingCards:", remainingCards);
         const randomCard = remainingCards[Math.floor(Math.random() * remainingCards.length)];
-
         const newCards = [...cards];
         newCards[idx] = randomCard;
         setCards(newCards);
@@ -54,21 +43,26 @@ function TarotReading() {
 
     return (
         <>
-            <section className='w-full mt-18'>
-                <h3 className=' bg-midnight text-mustard py-20 space-y-4 px-20 -mx-20 md:text-2xl md:-mx-20 xl:-mx-80 md:py-16 md:px-20 xl:px-80 xl:py-24'>Ven y explora tu <span className='font-semibold underline'>pasado</span>, comprende tu <span className='font-semibold underline'>presente</span> y descubre tu <span className='font-semibold underline'>futuro</span>.</h3>
-                <div className='my-18'>
+            <section className='w-full mt-18 text-mustard md:text-2xl'>
+                <h3 className=' bg-midnight  py-20 space-y-4 px-20 -mx-20 md:text-2xl md:-mx-20 xl:-mx-80 md:py-16 md:px-20 xl:px-80 xl:py-24'>Ven y explora tu <span className='font-semibold underline'>pasado</span>, comprende tu <span className='font-semibold underline'>presente</span> y descubre tu <span className='font-semibold underline'>futuro</span>.</h3>
+
+                <div>
                     {cards.map((card, idx) => (
-                        <div key={idx} className="w-3xs rounded-lg border-4 border-solid border-mustard cursor-pointer mt-16" onClick={() => handleClick(idx)}>
+                        <div key={idx} className="rounded-lg border-4 border-solid border-mustard  my-18" >
                             <h3 className='bg-midnight text-mustard text-center font-bold py-4 uppercase text-2xl border-b-4 border-mustard rounded-b-lg'>{titleCard[idx]}</h3>
-                            {card ? <img className='border-t-4 border-mustard rounded-t-lg' src={card.arcaneImage.imageSrc} alt={card.arcaneName} /> : <p className='bg-midnight text-mustard text-center font-bold py-4'>Haz clic para conocer tu {titleCard[idx]}.</p>}
+                            {card ? (
+                                <div className='p-18 bg-midnight flex flex-col justify-center'>
+                                    <CardDetailArcane cardArcane={card} />
+                                </div>
+                            ) : (
+                                <p className='bg-midnight text-mustard text-center font-bold py-4 cursor-pointer' onClick={() => handleClick(idx)}>Haz clic para conocer tu {titleCard[idx]}.</p>
+                            )}
                         </div>
                     ))}
                 </div>
-                <Outlet />
             </section>
         </>
-
-    );
+    )
 }
 
 export default TarotReading;
