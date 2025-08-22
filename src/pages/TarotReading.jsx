2a071f0173293f7e426df060f08dea3c2f649efc
marianyarj <marianyarj@gmail.com>
2025-08-22 11:10:24 +0200

    style: update responsive design for tarotReading page

--- src/pages/TarotReading.jsx
+++ src/pages/TarotReading.jsx
@@ -58,26 +58,27 @@ function TarotReading() {
 
     return (
         <>
-            <section className='w-full mt-18 text-mustard md:text-2xl'>
-                <h3 ref={titleRef} className=' bg-midnight  py-20 space-y-4 px-20 -mx-20 md:text-2xl md:-mx-20 xl:-mx-80 md:py-16 md:px-20 xl:px-80 xl:py-24'>Ven y explora tu <span className='font-semibold underline'>pasado</span>, comprende tu <span className='font-semibold underline'>presente</span> y descubre tu <span className='font-semibold underline'>futuro</span>.</h3>
+            <section className='w-full mt-18 text-mustard text-xl'>
+                <h3 ref={titleRef} className='bg-midnight md:text-2xl space-y-4 p-16 sm:p-20 md:px-32 lg:px-48 xl:px-80 2xl:px-120'>Ven y explora tu <span className='font-semibold underline'>pasado</span>, comprende tu <span className='font-semibold underline'>presente</span> y descubre tu <span className='font-semibold underline'>futuro</span>.</h3>
 
-                <div className={`mt-18 mb-12 flex justify-end ${fixedButton ? 'fixed top-4 right-0 z-50 pr-20 md:pr-40 lg:pr-60 xl:pr-80' : ''}`}>
+                <div className={`mt-18 flex justify-end mr-4 xl:px-35 ${fixedButton ? 'fixed top-4 right-0 z-50' : ''}`}>
                     <button
                         onClick={() => {
                             setCards([null, null, null]);
                             setSelectedGoddess([null, null, null]);
                         }}
-                        className='bg-midnight text-mustard hover:bg-mustard hover:text-midnight font-bold border-2 border-solid border-mustard cursor-pointer py-3 px-6 rounded-lg'
+                        className='hover:bg-midnight hover:text-mustard bg-mustard text-midnight font-bold border-2 border-solid border-mustard cursor-pointer py-3 px-6 rounded-lg'
                     >
                         Nueva lectura
                     </button>
                 </div>
                 <div>
                     {cards.map((card, idx) => (
-                        <div key={idx} className="rounded-lg border-4 border-solid border-mustard  my-18" >
-                            <h3 className='bg-midnight text-mustard text-center font-bold py-4 uppercase text-2xl border-b-4 border-mustard rounded-b-lg'>{titleCard[idx]}</h3>
+                        <div key={idx} className="py-10 sm:mx-20 md:mx-32 lg:mx-48 xl:mx-72" >
+                            <h3 className='bg-midnight text-mustard text-center font-bold py-4 uppercase text-2xl border-b-4 border-mustard rounded-tl-xl rounded-b-lg'>{titleCard[idx]}</h3>
                             {card ? (
-                                <div className='p-18 bg-midnight flex flex-col justify-center'>
+                                <div className='p-18 lg:p-12 xl:p-20 bg-midnight flex flex-col justify-center rounded-b-xl'>
+
                                     <CardDetailArcane cardArcane={card} />
                                     <div className='mt-8 md:mt-12 xl:mt-16 text-center'>
                                         <button
@@ -90,11 +91,14 @@ function TarotReading() {
                                         >
                                             Conocer la Diosa
                                         </button>
-                                        {selectedGoddess[idx] && <CardDetailGoddess cardGoddess={selectedGoddess[idx]} />}
+                                        <div className='mt-18'>
+                                            {selectedGoddess[idx] && <CardDetailGoddess cardGoddess={selectedGoddess[idx]} />}
+                                        </div>
+
                                     </div>
                                 </div>
                             ) : (
-                                <p className='bg-midnight text-mustard text-center font-bold py-4 cursor-pointer' onClick={() => handleClick(idx)}>Haz clic para conocer tu {titleCard[idx]}.</p>
+                                <p className='bg-midnight text-mustard text-center font-bold py-4 cursor-pointer rounded-br-xl' onClick={() => handleClick(idx)}>Haz clic para conocer tu {titleCard[idx]}.</p>
                             )}
                         </div>
 
