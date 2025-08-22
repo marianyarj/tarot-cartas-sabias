7e73ee9a86b710e893fcb932a0cf0ea990dbc817
marianyarj <marianyarj@gmail.com>
2025-08-22 15:57:31 +0200

    fix: router file name same case

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import { RouterProvider } from 'react-router-dom';
-import routerTarot from '../src/router/router';
+import routerTarot from './router/router';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
