6fe74baabfade4f76f596cc40ca82d1429da6f5f
marianyarj <marianyarj@gmail.com>
2025-08-22 11:09:01 +0200

    style: update responsive design for footerbar component

--- src/components/FooterBar.jsx
+++ src/components/FooterBar.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const FootBar = () => {
     return (
         <>
-            <div className='border-t-2 border-mustard bg-midnight py-12 -mx-20 md:-mx-40 lg:mx-60 xl:-mx-80'>
-                <section className='md:text-2xl text-mustard font-bold text-center'>
+            <section className='w-full border-t-2 border-mustard'>
+                <div className='bg-midnight text-mustard text-center space-y-4 text-xl md:text-2xl p-16 sm:p-20 md:px-32 lg:px-48 xl:px-80 2xl:px-120'>
                     <p>Este proyecto ha sido desarrollado como parte del programa de formación de <a href="https://factoriaf5.org/" target="_blank">FactoriaF5</a></p>
                     <p>© 2025 <a href="https://github.com/marianyarj" target="_blank">Mariany</a> | <a href="https://github.com/CeliaMi/stem-tarot/blob/main/README.md" target="_blank">Tarot STEM</a></p>
                     <div className='flex justify-center'>
@@ -12,8 +12,8 @@ const FootBar = () => {
                             src="https://femcoders.factoriaf5.org/wp-content/uploads/2021/12/Logo-FemCodersF5-color-300x224.png"
                             alt="logo femcoders factoriaf5" />
                     </div>
-                </section>
-            </div>
+                </div>
+            </section>
         </>
     )
 }
