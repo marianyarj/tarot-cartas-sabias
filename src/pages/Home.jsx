f58bea6ece1a1d9b06d14c15c7b86196fb7da15b
marianyarj <marianyarj@gmail.com>
2025-08-22 11:08:22 +0200

    style: update responsive design for home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -27,12 +27,20 @@ function Home() {
     return (
         <>
             <section className='w-full mt-18'>
-                <div className='bg-midnight text-mustard py-20 space-y-4 px-20 -mx-20 md:text-2xl md:-mx-20 xl:-mx-80 md:py-16 md:px-20 xl:px-80 xl:py-24'>
-                    <h2 >
-                        Ven a conocer el mundo de las mujeres que han dejado huella y siguen haciendo historia en la tecnología y ciencia.</h2>
-                    <h2 >Con Cartas Sábias podrás explorar una baraja de tarot para descubrir a estas Diosas contemporáneas.</h2>
-                    <h2 > Solo tienes que hacer clic en una carta para conocer más detalles sobre ella.</h2>
+
+                <div className='bg-midnight text-mustard space-y-4 text-xl md:text-2xl p-16 sm:p-20 md:px-32 lg:px-48 xl:px-80 2xl:px-120'>
+
+                    <h2>
+                        Ven a conocer el mundo de las mujeres que han dejado huella y siguen haciendo historia en la tecnología y ciencia.
+                    </h2>
+                    <h2>
+                        Con Cartas Sábias podrás explorar una baraja de tarot para descubrir a estas Diosas contemporáneas.
+                    </h2>
+                    <h2>
+                        Solo tienes que hacer clic en una carta para conocer más detalles sobre ella.
+                    </h2>
                 </div>
+
             </section>
             <div id="allCards">
                 <CardGrid cards={cardsTarot} onCardClick={cardClick} showName={true} />
