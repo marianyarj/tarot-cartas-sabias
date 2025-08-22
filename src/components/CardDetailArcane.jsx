f9ded602b736f8c0ce1f2864f88a5d6d3bc5ec11
marianyarj <marianyarj@gmail.com>
2025-08-22 11:09:48 +0200

    style: update responsive design for detailArcane component

--- src/components/CardDetailArcane.jsx
+++ src/components/CardDetailArcane.jsx
@@ -16,11 +16,11 @@ function CardDetailArcane({ cardArcane }) {
                     ))}
                 </h2>
                 <article className='row-start-2 flex flex-col md:flex-row items-center gap-6 mt-8'>
-                    <div className='flex flex-col items-center space-y-2'>
+                    <div className='flex flex-col items-center space-y-2 md:mt-18'>
                         <h3 className='text-2xl'>Arcano {cardArcane.arcaneNumber}</h3>
                         <img src={cardArcane.arcaneImage.imageSrc} alt={cardArcane.arcaneName} className='w-3xs rounded-lg border-4 border-solid border-mustard' />
                     </div>
-                    <p className='flex-1 mt-4  md:text-xl md:ml-6 2xl:text-2xl '>{cardArcane.arcaneDescription}</p>
+                    <p className='flex-1 mt-4  md:text-xl 2xl:text-2xl md:mt-18'>{cardArcane.arcaneDescription}</p>
                 </article>
             </section>
         </>
